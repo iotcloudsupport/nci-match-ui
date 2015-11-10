e6001cc0f76d0d362335c75eacce1764c3c1cb0e
mjhendrik <matti.hendriksson@nih.gov>
2015-11-10 14:23:50 -0500

    New Addtions to angular table

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -39,23 +39,70 @@ function MainCtrl() {
  */
 var ClickCtrl = angular.module('ClickCtrl',[]);
 
+//$scope.loadData = function()
+
+//$scope.loadData = function() {
+//    var URL = "http://localhost:4567/patientSpecimenTrackingSummary";
+//    var response = $http.get(URL);
+//    response.success(function(data, status, headers, config) {
+//        $scope.profile = data;
+//        alert("Ok.");
+//
+//    });
+//
+//    response.error(function(data, status, headers, config) {
+//        alert("Error.");
+//    });
+//
+//};
+
+
 function handleAnchorClick($scope, $http) {
-    var URL = "http://localhost:4567/patientSpecimenTrackingSummary";
 
-    $scope.click = function() {
-        var response = $http.get(URL);
-        response.success(function(data, status, headers, config) {
-            $scope.profile = data;
-            alert("Ok.");
+    var URL = "http://localhost:4567/generateReport?name=screenVsEnrolled";
+    //var URL = "http://localhost:4567/patientSpecimenTrackingSummary";
+    //var URL = "http://localhost:8080/match/common/rs/getBasicPatientsData"
 
-        });
+    $scope.loadData = function () {
+        $http.get(URL)
+            .success(function (data, status, headers, config) {
+                $scope.jsonData = data;
+            })
+            .error(function (data, status, header, config) {
+                $scope.ResponseDetails = "Data: " + data +
+                    "<br />status: " + status +
+                    "<br />headers: " + jsonFilter(header) +
+                    "<br />config: " + jsonFilter(config);
+            });
 
-        response.error(function(data, status, headers, config) {
-            alert("Error.");
-        });
-
+        //return $scope.jsonData;
     };
 }
+
+
+
+    //function handleAnchorClick($scope, $http) {
+    //    //var URL = "http://localhost:4567/patientSpecimenTrackingSummary";
+    //
+    //    $scope.loadData = function() {
+    //        //var URL = "http://localhost:4567/patientSpecimenTrackingSummary";
+    //        var URL = "http://localhost:8080/match/common/rs/getBasicPatientsData";
+    //
+    //        var response = $http.get(URL);
+    //        response.success(function(data, status, headers, config) {
+    //
+    //            alert("Ok")
+    //            $scope.jsonData = data;
+    //
+    //
+    //        });
+    //
+    //        response.error(function(data, status, headers, config) {
+    //            alert("Error.");
+    //        });
+    //
+    //    };
+    //}
 
 
 
