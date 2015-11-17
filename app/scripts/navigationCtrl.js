7e90c89526c99b1000a09371c67872aa93803f11
mjhendrik <matti.hendriksson@nih.gov>
2015-11-17 12:40:09 -0500

    Edits in nav tabs and tables. Improvement

--- app/scripts/navigationCtrl.js
+++ app/scripts/navigationCtrl.js
@@ -21,29 +21,64 @@
 //        return viewLocation === $location.path();
 //    };
 //}
+//var app = angular.module("app", []);
+//app.controller("activeCtrl", function ($scope) {
+//    $scope.data = {};
+//    $scope.data.isVisible = true;
+//});
 
+
+//var app = angular.module("app", []);
+//app.controller("activeCtrl", function ($scope) {
+//    $scope.data = {};
+//    $scope.data.case = true;
+//});
+
+
+//var activeCtrl = angular.module('activeCtrl',[]);
+//    function activeLink($scope) {
+//
+//    alert("pop")
+//
+//    $scope.activeTemplate='views/common/reportboard1.html'
+//}
+
+//$scope.nav = function(path) {
+//    $scope.filePath = path;
+//};
 
 var navCtrl = angular.module('navCtrl',[]);
-
 function toLink($scope) {
 
     $scope.linkSite = function(path) {
         $scope.isActive = function (viewLocation) {
-            alert("pop")
             return viewLocation === $location.path();
         };
 
     }
-
-    //alert($scope)
-
-    //$scope.tpl = {};
-    //$scope.tpl.contentUrl = '/views/common/reportboard1.html';
 }
+
+var activeCtrl = angular.module("activeCtrl", []);
+function activeLink($scope) {
+
+    $scope.activateLink = function (id) {
+        //$scope.activeTemplate='views/common/dashboard.html'
+
+        $scope.data = {};
+
+        $scope.pFilter = id;
+
+    };
+}
+
+
+
+
 
 angular
     .module('inspinia')
     .controller('navCtrl', toLink)
+    .controller('activeCtrl', activeLink)
     //.controller('ctrlB', function($scope) {
     //    $scope.v = "Hello from controller B";
     //});
