4d7ad9c71854ad3601154e85a2dcd72fc5d9f04d
mjhendrik <matti.hendriksson@nih.gov>
2015-12-23 10:56:04 -0500

    New additions to Jenkins test. Edit 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,22 @@ module.exports = function (grunt) {
         dist: 'dist'
     };
 
+    // Command line options
+    var cliOptions = {
+        hostname: grunt.option('hostname') || 'localhost',
+        port: grunt.option('port') || '8080',
+        contextPath: grunt.option('contextPath') || '/match',
+        filepath: '<%= appConfig.dist %>/' + (grunt.option('filename') || 'match-ui.tgz'),
+        guiVersion: grunt.option('guiVersion') || '1.1.0',
+        guiVersionFunc: function() {
+            return 'built on ' + new Date().getFullYear() + '-' +
+                (((new Date().getMonth()+1) < 10)?'0':'') + (new Date().getMonth()+1) + '-' +
+                ((new Date().getDate() < 10)?'0':'') + (new Date().getDate()) + ' ' +
+                ((new Date().getHours() < 10)?'0':'') + new Date().getHours() + ':' +
+                ((new Date().getMinutes() < 10)?'0':'') + new Date().getMinutes();
+        }
+    };
+
     // Grunt configuration
     grunt.initConfig({
 
