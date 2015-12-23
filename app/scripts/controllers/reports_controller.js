24296fb40a6424c0c398c5076c8c2c213964947b
Shan Yang <shan.yang@nih.gov>
2015-12-23 17:16:20 -0500

    Added application/x-www-form-urlencoded as content-type when making rest call

--- app/scripts/controllers/reports_controller.js
+++ app/scripts/controllers/reports_controller.js
@@ -4,8 +4,11 @@ angular.module('reports.matchbox',[])
         $scope.reportList = []
 
         $scope.loadReportList = function() {
-            $http.get(matchConfig.reportApiBaseUrl + '/reportList')
-                .success(function (data) {
+             $.ajax({
+                type: 'GET',
+                url: matchConfig.reportApiBaseUrl + '/reportList',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded',},
+                success: function (data) {
                     $.each(data, function( key, value ) {
                         $scope.reportList.push({
                             'name': value.name,
@@ -16,10 +19,32 @@ angular.module('reports.matchbox',[])
                             'csvHref': matchConfig.reportApiBaseUrl + '/downloadReportFile?name=' + value.name + '&type=csv'
                         });
                     });
-                })
-                .error(function (data, status, header, config) {
-                    console.log(data + '|' + status + '|' + header + '|' + config);
-                });
+                },
+                error: function (request, textStatus, errorThrown) {
+                    alert(errorThrown)
+                    $('#alertMessage').hide();
+                    $('#errorMessage').show();
+                },
+                dataType: 'json'
+            });
+
+            // $http.get(matchConfig.reportApiBaseUrl + '/reportList')
+            // $http.get('http://127.0.0.1:4568/reportList')
+            //     .success(function (data) {
+            //         $.each(data, function( key, value ) {
+            //             $scope.reportList.push({
+            //                 'name': value.name,
+            //                 'displayName': value.displayName,
+            //                 'description': value.description,
+            //                 'excelHref': matchConfig.reportApiBaseUrl + '/downloadReportFile?name=' + value.name + '&type=excel',
+            //                 'jsonHref': matchConfig.reportApiBaseUrl + '/downloadReportFile?name=' + value.name + '&type=json',
+            //                 'csvHref': matchConfig.reportApiBaseUrl + '/downloadReportFile?name=' + value.name + '&type=csv'
+            //             });
+            //         });
+            //     })
+            //     .error(function (data, status, header, config) {
+            //         console.log(data + '|' + status + '|' + header + '|' + config);
+            //     });
         }
 
     });
