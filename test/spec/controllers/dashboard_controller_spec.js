4c8933f4d3ebd9ba38682965c3eab7d3f255af16
mjhendrik <matti.hendriksson@nih.gov>
2016-01-21 13:05:06 -0500

    Dashboard unit test added to testing dir

---
+++ test/spec/controllers/dashboard_controller_spec.js
@@ -0,0 +1,165 @@
+describe('Controller: dashboard Controller', function () {
+
+    beforeEach(module('config.matchbox', 'http.matchbox', 'dashboard.matchbox'));
+
+    var DashCtrl,
+        httpBackend,
+        scope;
+
+    beforeEach(inject(function ($controller,
+                                $rootScope,
+                                _matchApi_,
+                                $httpBackend) {
+        scope = $rootScope.$new();
+        httpBackend = $httpBackend;
+
+        DashCtrl = $controller('DashboardPendingReviewController', {
+            $scope: scope,
+            DTOptionsBuilder: {
+                newOptions: function () {
+                    return {
+                        withDisplayLength: function (length) {
+                            // This is a mock function of the DTOptionsBuilder
+                        },
+                        withOption: function (bLengthChange) {
+                            // This is a mock function of the DTOptionsBuilder
+                        }
+                    };
+                }
+            },
+            DTColumnDefBuilder: {
+                newColumnDef: function () {
+                    return {
+                        notVisible: function () {}
+                    };
+                }
+            },
+            matchApi: _matchApi_,
+        });
+
+
+    }));
+
+    afterEach(function () {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should populate the dashboard Variant Reports list with  on a success response', function() {
+        httpBackend.when('GET', 'http://server:80/match/common/rs/getPatientsWithPendingVariantReport')
+            .respond([
+                {
+                    "patientSequenceNumber":"10375",
+                    "biopsySequenceNumber":"N-15-00005",
+                    "currentPatientStatus":"REGISTRATION",
+                    "specimenReceivedDate":1449922209071,
+                    "molecularSequenceNumber":"10375_1000_N-15-00005",
+                    "jobName":"testjob6",
+                    "ngsStatus":"PENDING",
+                    "ngsDateReceived":1446758737780,
+                    "daysPending":38,
+                    "location":"Boston"
+                },
+                {
+                    "patientSequenceNumber":"10376",
+                    "biopsySequenceNumber":"N-15-00006",
+                    "currentPatientStatus":"REGISTRATION",
+                    "specimenReceivedDate":1449922209078,
+                    "molecularSequenceNumber":"10376-15-00005",
+                    "jobName":"testjob3",
+                    "ngsStatus":"PENDING",
+                    "ngsDateReceived":1446758737780,
+                    "daysPending":18,
+                    "location":"Boston"
+                },
+                {
+                    "patientSequenceNumber":"10377",
+                    "biopsySequenceNumber":"N-15-00007",
+                    "currentPatientStatus":"REGISTRATION",
+                    "specimenReceivedDate":1449922205071,
+                    "molecularSequenceNumber":"10377-15-00005",
+                    "jobName":"testjob8",
+                    "ngsStatus":"PENDING",
+                    "ngsDateReceived":1446758737780,
+                    "daysPending":5,
+                    "location":"Boston"
+                }
+            ]);
+        scope.loadPatientVariantReportsList();
+        httpBackend.flush();
+
+        expect(scope.pendingVariantReportList.length).toBe(3);
+        expect(scope.pendingVariantReportList[0].patientSequenceNumber).toBe('10375');
+        expect(scope.pendingVariantReportList[1].biopsySequenceNumber).toBe('N-15-00006');
+        expect(scope.pendingVariantReportList[2].jobName).toBe('testjob8');
+    });
+
+    it('should not populate the Variant Reports list on an error response', function() {
+        httpBackend.when('GET', 'http://server:80/match/common/rs/getPatientsWithPendingVariantReport').respond(500);
+        scope.loadPatientVariantReportsList();
+        httpBackend.flush();
+
+        expect(scope.pendingVariantReportList.length).toBe(0);
+    });
+
+
+
+
+    it('should populate the dashboard Patient Pending Assignment list with  on a success response', function() {
+        httpBackend.when('GET', 'http://server:80/match/common/rs/getPatientsWithPendingAssignmentReport')
+            .respond([
+                {
+                    "patientSequenceNumber":"10375",
+                    "biopsySequenceNumber":"N-15-00005",
+                    "currentPatientStatus":"REGISTRATION",
+                    "specimenReceivedDate":1449922209071,
+                    "molecularSequenceNumber":"10375_1000_N-15-00005",
+                    "jobName":"testjob6",
+                    "ngsStatus":"PENDING",
+                    "ngsDateReceived":1446758737780,
+                    "daysPending":38,
+                    "location":"Boston"
+                },
+                {
+                    "patientSequenceNumber":"10376",
+                    "biopsySequenceNumber":"N-15-00006",
+                    "currentPatientStatus":"REGISTRATION",
+                    "specimenReceivedDate":1449922209078,
+                    "molecularSequenceNumber":"10376-15-00005",
+                    "jobName":"testjob3",
+                    "ngsStatus":"PENDING",
+                    "ngsDateReceived":1446758737780,
+                    "daysPending":18,
+                    "location":"Boston"
+                },
+                {
+                    "patientSequenceNumber":"10377",
+                    "biopsySequenceNumber":"N-15-00007",
+                    "currentPatientStatus":"REGISTRATION",
+                    "specimenReceivedDate":1449922205071,
+                    "molecularSequenceNumber":"10377-15-00005",
+                    "jobName":"testjob8",
+                    "ngsStatus":"PENDING",
+                    "ngsDateReceived":1446758737780,
+                    "daysPending":5,
+                    "location":"Boston"
+                }
+            ]);
+        scope.loadPatientPendingAssignmentReportsList();
+        httpBackend.flush();
+
+        expect(scope.pendingAssignmentReportList.length).toBe(3);
+        expect(scope.pendingAssignmentReportList[0].patientSequenceNumber).toBe('10375');
+        expect(scope.pendingAssignmentReportList[1].biopsySequenceNumber).toBe('N-15-00006');
+        expect(scope.pendingAssignmentReportList[2].jobName).toBe('testjob8');
+    });
+
+    it('should not populate the Patient Pending Assignment on an error response', function() {
+        httpBackend.when('GET', 'http://server:80/match/common/rs/getPatientsWithPendingAssignmentReport').respond(500);
+        scope.loadPatientPendingAssignmentReportsList();
+        httpBackend.flush();
+
+        expect(scope.pendingAssignmentReportList.length).toBe(0);
+    });
+
+});
