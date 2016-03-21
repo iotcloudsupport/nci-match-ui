a1ce3def5fcd2b867e6ff3909e659f2c318f1eb7
mjhendrik <matti.hendriksson@nih.gov>
2016-03-21 11:56:30 -0400

    Services folder added to the controllers

---
+++ app/scripts/services/match_service.js
@@ -0,0 +1,18 @@
+angular.module('service.matchbox', [])
+    .factory('idService', function() {
+    var productList = [];
+
+    var addProduct = function(newObj) {
+        productList = [];
+        productList.push(newObj);
+    };
+
+    var getProducts = function() {
+        return productList;
+    };
+
+    return {
+        addProduct: addProduct,
+        getProducts: getProducts
+    };
+});
