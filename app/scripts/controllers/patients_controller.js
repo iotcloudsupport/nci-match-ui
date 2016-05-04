43a47fb6b519490c9d8a4098b05e43dde4ae0d6b
Rick Zakharov <rick.zakharov@gmail.com>
2016-05-03 22:44:14 -0400

    Working on checkbox with confirmation

--- app/scripts/controllers/patients_controller.js
+++ app/scripts/controllers/patients_controller.js
@@ -17,30 +17,6 @@ angular.module('patients.matchbox',[])
                 .getPatientListData()
                 .then(function (d) {
                     $scope.patientList = d;
-                    
-                    // angular.forEach(d.data, function (value, key) {
-                    //     var treatmentArmId = '-';
-                    //     var treatmentArmVersion = '-';
-                    //     var treatmentArm = null;
-                    //     if (value.currentTreatmentArm &&
-                    //         value.currentTreatmentArm.id &&
-                    //         value.currentTreatmentArm.version) {
-                    //         treatmentArmId = value.currentTreatmentArm.id;
-                    //         treatmentArmVersion =  value.currentTreatmentArm.version;
-                    //         treatmentArm = value.currentTreatmentArm.id + ' (' + treatmentArmVersion + ')'
-                    //     }
-                    //     $scope.patientList.push({
-                    //         'patientSequenceNumber': value.patientSequenceNumber,
-                    //         'currentStatus': value.currentStatus,
-                    //         'currentStepNumber': value.currentStepNumber,
-                    //         'diseases': value.diseases,
-                    //         'currentTreatmentArmId': treatmentArmId,
-                    //         'currentTreatmentArmVersion': treatmentArmVersion,
-                    //         'currentTreatmentArm': treatmentArm,
-                    //         'registrationDate': value.registrationDate,
-                    //         'offTrialDate': value.offTrialDate
-                    //     });
-                    // });
                 });
         };
     });
