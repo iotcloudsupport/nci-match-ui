77b91fffc711da979c8e0c11c361a5033fd4af07
dumenee <erika.dumene@nih.gov>
2016-05-13 14:59:22 -0400

    initial connection of web services to UI, adding unit test coverage

--- app/scripts/controllers/treatment_arms_controller.js
+++ app/scripts/controllers/treatment_arms_controller.js
@@ -12,9 +12,32 @@ angular.module('treatment-arms.matchbox',[])
             treatmentArmApi
                 .getTreatmentArms()
                 .then(function(d) {
+                    console.log(d);
                     $scope.treatmentArmList = d.data;
+                    console.log(d.data);
+
                     console.log($scope);
                 });
+                /*.then(function(d) {
+                    angular.forEach(d.data, function (value) {
+                    //$.each(d.data, function( key, value ) {
+
+                        $scope.treatmentArmList .push({
+                            name: 'EAY131-T'
+                            /*name: value.data.treatment_arm_id,
+                            current_patients: value.data.current_patients,
+                            former_patients: value.data.former_patients,
+                            not_enrolled_patients: value.data.not_enrolled_patients,
+                            pending_patients: value.data.pending_patients,
+                            treatment_arm_status: value.data.treatment_arm_status,
+                            date_created: value.data.date_created,
+                            date_opened: value.data.date_opened,
+                            date_closed: value.date.date_closed,
+                            date_suspended: value.data.date_suspended*/
+                        /*});
+
+                    });
+                });*/
         };
 
 
