95f36bea845d4dbe6606a6e7365901604afbd40e
dumenee <erika.dumene@nih.gov>
2016-05-13 15:07:29 -0400

    clean up code

--- app/scripts/controllers/treatment_arms_controller.js
+++ app/scripts/controllers/treatment_arms_controller.js
@@ -18,26 +18,7 @@ angular.module('treatment-arms.matchbox',[])
 
                     console.log($scope);
                 });
-                /*.then(function(d) {
-                    angular.forEach(d.data, function (value) {
-                    //$.each(d.data, function( key, value ) {
-
-                        $scope.treatmentArmList .push({
-                            name: 'EAY131-T'
-                            /*name: value.data.treatment_arm_id,
-                            current_patients: value.data.current_patients,
-                            former_patients: value.data.former_patients,
-                            not_enrolled_patients: value.data.not_enrolled_patients,
-                            pending_patients: value.data.pending_patients,
-                            treatment_arm_status: value.data.treatment_arm_status,
-                            date_created: value.data.date_created,
-                            date_opened: value.data.date_opened,
-                            date_closed: value.date.date_closed,
-                            date_suspended: value.data.date_suspended*/
-                        /*});
-
-                    });
-                });*/
+                
         };
 
 
