4e11ecf4ede4db0da65972f4353588311510c198
Rick Zakharov <rick.zakharov@gmail.com>
2016-05-17 18:21:31 -0400

    Completed writing unit tests for all filters

--- app/scripts/filters/match_filters.js
+++ app/scripts/filters/match_filters.js
@@ -6,7 +6,8 @@
         .filter('status', status)
         .filter('analysisStatus', analysisStatus)
         .filter('assayStatus', assayStatus)
-        .filter('concordance', concordance);
+        .filter('concordance', concordance)
+        .filter('irsample', irsample);
 
     function isNotString(text) { 
         return typeof text !== "string";
