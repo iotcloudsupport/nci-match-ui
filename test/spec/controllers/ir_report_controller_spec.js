655d4956744c2f68852eaf03af0d1be20362c36f
mjhendrik <matti.hendriksson@nih.gov>
2016-05-25 08:59:27 -0400

    Ir report controller unit test edited

--- test/spec/controllers/ir_report_controller_spec.js
+++ test/spec/controllers/ir_report_controller_spec.js
@@ -2,23 +2,7 @@ describe('Controller: Ir Report Controller', function () {
 
     'use strict'
 
-    function getTestData() {
-
-        // "id" : 55b8f62a00922fdd29b46e7e,
-        //     "hostName" : NCIAS-D1356,
-        //     "ipAddress" : 10.133.226.13,
-        //     "status" : Lost contact! Last heartbeat was sent 99598 minutes ago,
-        //     "lastContactDate" : 1458143402201,
-        //     "location" : ,
-        // "externalIpAddress" : 10.133.226.13,
-        //     "dbReport" : dbReport,
-        //     "dataFile" : dataFile,
-        //     "logFile" : logFile,
-        //     "dbReportPath" : /local/content/wildfly/standalone-18080/log/iruploader/10-133-226-13/dbreport-1458140462188.json,
-        //     "dataFilePath" : /local/content/wildfly/standalone-18080/log/iruploader/10-133-226-13/data-1458143402180.txt,
-        //     "logFilePath" : /local/content/wildfly/standalone-18080/log/iruploader/10-133-226-13/iruploader-1458140462188.log,
-        //     "lastNotificationDate" : 1448918220981
-
+    function getHeartBeatTestData() {
 
         var data = [
             {
@@ -53,6 +37,67 @@ describe('Controller: Ir Report Controller', function () {
         return data;
     }
 
+    // "id": "MoCha_15",
+    //     "molecularSequenceNumber": "SampleControl_MoCha_15",
+    //     "dateCreated": 1452022386103,
+    //     "dateReceived": null,
+    //     "site": "MoCha",
+    //     "siteIpAddress": "129.43.127.133",
+    //     "positiveControlVersion": 0,
+    //     "positiveControlDateLoaded": null,
+    //     "status": null,
+    //     "comment": null,
+    //     "passed": false,
+    //     "nextGenerationSequence": null
+
+
+    function getSampleControlTestData() {
+
+        var data = [
+            {
+                molecularSequenceNumber: "SampleControl_MoCha_1",
+                dateCreated: '1452022386103',
+                dateReceived: null,
+                site: "MoCha",
+                siteIpAddress: "119.55.127.133",
+                positiveControlVersion: 0,
+                positiveControlDateLoaded: null,
+                status: null,
+                comment: null,
+                passed: false,
+                nextGenerationSequence: null
+            },
+            {
+                molecularSequenceNumber: "SampleControl_MoCha_15",
+                dateCreated: '1452022386103',
+                dateReceived: null,
+                site: "MoCha",
+                siteIpAddress: "129.43.127.03",
+                positiveControlVersion: 0,
+                positiveControlDateLoaded: null,
+                status: null,
+                comment: null,
+                passed: false,
+                nextGenerationSequence: null
+            },
+            {
+                molecularSequenceNumber: "SampleControl_MoCha_25",
+                dateCreated: '1452022386103',
+                dateReceived: null,
+                site: "MoCha",
+                siteIpAddress: "159.43.127.133",
+                positiveControlVersion: 0,
+                positiveControlDateLoaded: null,
+                status: null,
+                comment: null,
+                passed: false,
+                nextGenerationSequence: null
+            }
+        ];
+
+        return data;
+    }
+
     beforeEach(module('config.matchbox', 'http.matchbox', 'iradmin.matchbox'));
     var $scope;
     var irAdminApi;
@@ -60,11 +105,13 @@ describe('Controller: Ir Report Controller', function () {
     var httpBackend;
     var $q;
     var deferred;
-    var testData;
+    var testHeartBeatData;
+    var testSampleControlData;
     var $log;
 
     beforeEach(inject(function (_$rootScope_, $controller, $httpBackend, _irAdminApi_, _$log_, _$q_, _$http_) {  //$scope, $http, $window, DTOptionsBuilder, DTColumnDefBuilder, irAdminApi
-        testData = getTestData();
+        testHeartBeatData = getHeartBeatTestData();
+        testSampleControlData = getSampleControlTestData();
         deferred = _$q_.defer();
         $log = _$log_;
         $q = _$q_;
@@ -98,8 +145,8 @@ describe('Controller: Ir Report Controller', function () {
         });
     });
 
-    it('should call api load method and have the list populated', function () {
-        deferred.resolve(testData);
+    it('should call api load method and have the Heartbeat list populated', function () {
+        deferred.resolve(testHeartBeatData);
 
         spyOn(irAdminApi, 'loadHeartBeatList').and.returnValue(deferred.promise);
 
@@ -107,6 +154,18 @@ describe('Controller: Ir Report Controller', function () {
         $scope.$apply();
 
         expect(irAdminApi.loadHeartBeatList).toHaveBeenCalled();
+        expect($scope.irList.length).toBeLessThan(200);
+    });
+
+    it('should call api load method and have the Sample Control list populated', function () {
+        deferred.resolve(testSampleControlData);
+
+        spyOn(irAdminApi, 'loadSampleControlsList').and.returnValue(deferred.promise);
+
+        $scope.loadSampleControlsList();
+        $scope.$apply();
+
+        expect(irAdminApi.loadSampleControlsList).toHaveBeenCalled();
         expect($scope.irList.length).toBeLessThan(200);
     });
 });
