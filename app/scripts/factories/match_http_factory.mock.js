856cf6821d4a66ddbf4dc58581ddc8744024fde9
Rick Zakharov <rick.zakharov@gmail.com>
2016-06-02 12:19:41 -0400

    Removed extra documents from the mock model

--- app/scripts/factories/match_http_factory.mock.js
+++ app/scripts/factories/match_http_factory.mock.js
@@ -28,12 +28,7 @@
                     },
                     treatmentArm: 'EAY131-B',
                     assignment: { id: 123, reason: 'The patient arm match on variant indetifier [ABC, EDF]' },
-                    drugs: ['Aspirin', 'Motrin', 'Vitamin D'],
-                    documents: [
-                        { url: 'url1', title: 'Document 1' },
-                        { url: 'url2', title: 'Document 2' },
-                        { url: 'url3', title: 'Document 3' }
-                    ]
+                    drugs: ['Aspirin', 'Motrin', 'Vitamin D']
                 }
 
                 data.treatmentArms = {};
