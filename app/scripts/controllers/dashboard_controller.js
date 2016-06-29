a2185b6e6d80cf11de737fefaaa7ebc7d60f56e1
dumenee <erika.dumene@nih.gov>
2016-06-29 09:09:34 -0400

    Changing TA names to APEC

--- app/scripts/controllers/dashboard_controller.js
+++ app/scripts/controllers/dashboard_controller.js
@@ -228,7 +228,7 @@ angular.module('dashboard.matchbox',[])
                 "pic": $scope.icons[2],
                 "status": $scope.status[2],
                 "patientId": '',
-                "treatmentArmId": 'EAY131-A',
+                "treatmentArmId": 'APEC1621-A',
                 "molecularId": '',
                 "biopsySequenceNumber": '',
                 "time": 1421175969643,
@@ -283,7 +283,7 @@ angular.module('dashboard.matchbox',[])
                 "pic": $scope.icons[2],
                 "status": $scope.status[2],
                 "patientId": '',
-                "treatmentArmId": 'EAY131-A',
+                "treatmentArmId": 'APEC1621-A',
                 "molecularId": '',
                 "biopsySequenceNumber": '',
                 "time": 1421162969643,
@@ -451,7 +451,7 @@ angular.module('dashboard.matchbox',[])
 
         $scope.loadTreatmentArmAccrual = function() {
 
-            armNames = ['EAY131-Q', 'EAY131-B', 'EAY131-H', 'EAY131-U', 'EAY131-E'];
+            armNames = ['APEC1621-Q', 'APEC1621-B', 'APEC1621-H', 'APEC1621-U', 'APEC1621-E'];
             armValues = [6, 3, 2, 2, 1];
 
             $scope.barData = {
