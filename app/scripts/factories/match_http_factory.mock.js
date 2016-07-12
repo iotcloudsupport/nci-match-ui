64a7cbfe67c14609ed2a62ca7034dde202db2238
dumenee <erika.dumene@nih.gov>
2016-07-12 09:11:38 -0400

    added calls to mock data for treatment arm, specimen tracking, and dashboard

--- app/scripts/factories/match_http_factory.mock.js
+++ app/scripts/factories/match_http_factory.mock.js
@@ -15,5 +15,45 @@
             function loadPatientList() {
                 return $http.get('data/patient_list.json');
             }
+
+            function loadTreatmentArmDetails(taId) {
+                return $http.get('data/treatment_arm_' + id + '.json');
+            }
+
+            function loadPatientsForTa(taid) {
+                return $http.get('data/treatment_arm_' + id + '_patients.json');
+            }
+
+            function loadSpecimenTrackingList() {
+                return $http.get('data/specimen_tracking_list.json');
+            }
+
+            function loadDashboardStatistics() {
+                return $http.get('data/dashboard_statistics.json');
+            }
+
+            function loadTreatmentArmAccrual() {
+                return $http.get('data/treatment_arm_accrual.json');
+            }
+            function loadChartjsDonutChart(){
+                return $http.get('data/dashboard_donut_chart.json');
+            }
+
+            function loadActivityList() {
+                return $http.get('data/dashboard_activity_feed.json');
+            }
+
+            function loadTissueVariantReportsList(){
+                return $http.get('data/dashboard_tissue_variant_reports.json');
+            }
+
+            function loadBloodVariantReportsList() {
+                return $http.get('data/dashboard_blood_variant_reports.json');
+            }
+
+            function loadPatientPendingAssignmentReportsList() {
+                return $http.get('data/dashboard_patient_assignment_reports.json');
+            }
+
         });
 } ());
