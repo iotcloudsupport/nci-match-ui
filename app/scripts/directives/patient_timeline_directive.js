be51c3890f5636674650a7660b35fca415ec08f5
Rick Zakharov <rick.zakharov@gmail.com>
2016-07-14 10:24:05 -0400

    MATCHKB-424 MATCH UI - Creating separate template for narrow activity feed

--- app/scripts/directives/patient_timeline_directive.js
+++ app/scripts/directives/patient_timeline_directive.js
@@ -4,13 +4,11 @@
     angular
         .module('matchbox')
         .directive('patientTimeline', patientTimeline);
-        
+
     /**
      * patientTimeline
      */
     function patientTimeline($compile, $http, $templateCache, $log) {
-        var directive = this;
-        
         var baseUrl = '/views/templates/patient_timeline_';
 
         /* sample data
@@ -24,56 +22,71 @@
         { type: 'system', dateTime: 'Sep 17, 2015, 10:00 AM GMT', status: 'error', details: 'Unable to process patient biopsy message. Please contact techincal support.' }
         
         */
-        
+
         var templateMap = {
-            'biopsy.assay': baseUrl + 'biopsy_assay.html',
-            'biopsy.assay_order': baseUrl + 'biopsy_assay_order.html',
-            'biopsy.assay_received': baseUrl + 'biopsy_assay_received.html',
-            'biopsy.variantReportReceived': baseUrl + 'biopsy_variantReportReceived.html',
-            'biopsy.variantReportShipped': baseUrl + 'biopsy_variantReportShipped.html',
-            'biopsy.pathology': baseUrl + 'biopsy_pathology.html',
-            'assignment': baseUrl + 'assignment_narrow.html',
-            'patient': baseUrl + 'patient.html',
-            'user': baseUrl + 'user.html',
-            'system': baseUrl + 'system.html'
+            'biopsy.assay.normal': baseUrl + 'biopsy_assay.html',
+            'biopsy.assay.narrow': baseUrl + 'biopsy_assay_narrow.html',
+            'biopsy.assay_order.normal': baseUrl + 'biopsy_assay_order.html',
+            'biopsy.assay_order.narrow': baseUrl + 'biopsy_assay_order_narrow.html',
+            'biopsy.assay_received.normal': baseUrl + 'biopsy_assay_received.html',
+            'biopsy.assay_received.narrow': baseUrl + 'biopsy_assay_received_narrow.html',
+            'biopsy.variantReportReceived.normal': baseUrl + 'biopsy_variantReportReceived.html',
+            'biopsy.variantReportReceived.narrow': baseUrl + 'biopsy_variantReportReceived_narrow.html',
+            'biopsy.variantReportShipped.normal': baseUrl + 'biopsy_variantReportShipped.html',
+            'biopsy.variantReportShipped.narrow': baseUrl + 'biopsy_variantReportShipped_narrow.html',
+            'biopsy.pathology.normal': baseUrl + 'biopsy_pathology.html',
+            'biopsy.pathology.narrow': baseUrl + 'biopsy_pathology_narrow.html',
+            'assignment.normal': baseUrl + 'assignment.html',
+            'assignment.narrow': baseUrl + 'assignment_narrow.html',
+            'patient.normal': baseUrl + 'patient.html',
+            'patient.narrow': baseUrl + 'patient_narrow.html',
+            'user.normal': baseUrl + 'user.html',
+            'user.narrow': baseUrl + 'user_narrow.html',
+            'system.normal': baseUrl + 'system.html',
+            'system.narrow': baseUrl + 'system_narrow.html'
         };
-        
-        var getTemplateLoader = function(type) {
-            if (type in templateMap) {
-                var url = templateMap[type];
-                return $http.get(url, {cache: $templateCache});
+
+        var getTemplateLoader = function (type, layout) {
+            var key = type + '.' + (layout ? layout : 'normal');
+
+            $log.debug('key = '+key);
+
+            if (key in templateMap) {
+                var url = templateMap[key];
+                return $http.get(url, { cache: $templateCache });
             } else {
                 $log.error('patient-timeline directive: invalid timeline type "' + type + '"');
             }
-        }
-        
+        };
+
         var defaultTemplate = '<div><p>Invalid Timeline Type "{{ timelineEvent.type }}"</p><p>{{ timelineEvent }}</p></div>';
 
-        var linker = function(scope, element, attrs) {
-            var loader = getTemplateLoader(scope.type);
-            
+        var linker = function (scope, element, attrs) {
+            var loader = getTemplateLoader(scope.type, scope.layout);
+
             if (!loader) {
                 element.html(defaultTemplate);
                 element.replaceWith($compile(element.html())(scope));
                 return;
             }
 
-            var promise = loader.success(function(html) {
+            var promise = loader.success(function (html) {
                 element.html(html);
             }).then(function (response) {
                 element.replaceWith($compile(element.html())(scope));
             });
-        }
-        
+        };
+
         return {
             restrict: 'E',
             scope: {
                 timelineEvent: '<',
                 type: '<',
-                isStepChanging: '<'
+                isStepChanging: '<',
+                layout: '<'
             },
             link: linker
         }
     }
-    
+
 } ());
