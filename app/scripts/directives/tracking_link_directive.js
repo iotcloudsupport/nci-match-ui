22b4c74412aecbb368f6b9f7b0382fa0cd457615
Rick Zakharov <rick.zakharov@gmail.com>
2016-07-20 13:45:15 -0400

    MATCHKB-396 Match UI – create a directive for tracking URL

---
+++ app/scripts/directives/tracking_link_directive.js
@@ -0,0 +1,46 @@
+(function () {
+    "use strict";
+
+    angular
+        .module('matchbox')
+        .directive('trackingLink', trackingLink);
+
+    function trackingLink($log) {
+        var controller = function () {
+            var vm = this;
+
+            vm.carriers = {
+                fedex: { url: 'http://www.fedex.com/Tracking?action=track&tracknumbers={trackingNumber}' },
+                ups: { url: 'http://wwwapps.ups.com/WebTracking/track?track=yes&trackNums;={trackingNumber}'},
+                usps: { url: 'https://tools.usps.com/go/TrackConfirmAction_input?qtc_tLabels1={trackingNumber}' },
+                lasership: { url: 'http://www.lasership.com/track/LSXXXXXXXX' },
+                dhl: { url: 'http://track.dhl-usa.com/TrackByNbr.asp?ShipmentNumber=XXXXXXXXXXXXXXXXX' }
+            };
+
+            vm.getTrackingUrl = function() {
+                if (vm.carrier in vm.carriers) {
+                    return vm.carriers[vm.carrier].url.replace(/{trackingNumber}/g, vm.trackingNumber);
+                } else {
+                    $log.error('Invalid carrier ' + vm.carrier);
+                    return '#';
+                }
+            }
+        };
+
+        /*jshint multistr: true */
+        var template = '<span><a href="{{vm.getTrackingUrl()}}" target="_blank">{{vm.trackingNumber}}</a></span>';
+
+        return {
+            bindToController: true,
+            restrict: 'EA',
+            template: template,
+            controller: controller,
+            controllerAs: 'vm',
+            scope: {
+                trackingNumber: '<',
+                carrier: '<'
+            }
+        }
+    }
+
+} ());
