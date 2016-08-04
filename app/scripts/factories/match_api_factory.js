fdb5655f84c83f5792ae1627c98ed9530d69693d
dumenee <erika.dumene@nih.gov>
2016-08-04 09:44:04 -0400

    update to TA details backend connection

--- app/scripts/factories/match_api_factory.js
+++ app/scripts/factories/match_api_factory.js
@@ -84,7 +84,7 @@
             }
 
             function loadPatientsForTa(name, stratum) {
-                return $http.get(matchConfig.treatmentArmApiBaseUrl + '/patientsOnTreatmentArm/' + name + '/' + stratum);
+                return $http.get(matchConfig.treatmentArmApiBaseUrl + '/patientsOnTreatmentArm/' + name); // + '/' + stratum);
             }
             // Treatment Arm API - END
 
