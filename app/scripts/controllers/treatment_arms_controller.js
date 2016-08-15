160e24dd5402f98c197f0b836b8eae5a8f30355e
Rick Zakharov <rick.zakharov@gmail.com>
2016-08-15 13:14:15 -0400

    Changed patien counts to integer on TA list page

--- app/scripts/controllers/treatment_arms_controller.js
+++ app/scripts/controllers/treatment_arms_controller.js
@@ -24,6 +24,21 @@
                         .loadTreatmentArmList()
                         .then(function (d) {
                             $scope.treatmentArmList = d.data;
+                            for (var i = 0; i < $scope.treatmentArmList.length; i++) {
+                                var ta = $scope.treatmentArmList[i];
+                                if (ta.current_patients !== undefined && ta.current_patients !== null) {
+                                    ta.current_patients = Math.floor(ta.current_patients);
+                                }
+                                if (ta.former_patients !== undefined && ta.former_patients !== null) {
+                                    ta.former_patients = Math.floor(ta.former_patients);
+                                }
+                                if (ta.not_enrolled_patients !== undefined && ta.not_enrolled_patients !== null) {
+                                    ta.not_enrolled_patients = Math.floor(ta.not_enrolled_patients);
+                                }
+                                if (ta.pending_patients !== undefined && ta.pending_patients !== null) {
+                                    ta.pending_patients = Math.floor(ta.pending_patients);
+                                }
+                            }
                         });
                 };
             }
