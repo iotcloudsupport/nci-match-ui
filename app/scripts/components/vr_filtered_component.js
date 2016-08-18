f94dba957f527e51cfd70c25809b5bcec86b3cfc
Rick Zakharov <rick.zakharov@gmail.com>
2016-08-18 15:38:08 -0400

    Added cosmic-link directive

--- app/scripts/components/vr_filtered_component.js
+++ app/scripts/components/vr_filtered_component.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
 
-    function VrFilteredReportController($scope, $element, $attrs, $log) {
+    function VrFilteredReportController($scope, $element, $attrs, $log, $window) {
         var ctrl = this;
 
         ctrl.confirmTitle = '';
@@ -13,12 +13,33 @@
             ctrl.gridOptions = {
                 data: ctrl.items
             };
-            $log.debug('Initialized VR with ' + ctrl.gridData.data.length);
         };
 
         ctrl.onItemConfirmed = function (item) {
             ctrl.onItemConfirmed(item);
         };
+
+        //COSMIC LINKS
+        ctrl.openCosmicGene = function (id) {
+            $window.open("http://cancer.sanger.ac.uk/cosmic/gene/overview?ln=" + id.toLowerCase(), "_blank");
+            $window.focus();
+        };
+
+        ctrl.openCosmicId = function (id) {
+            id = id.substring(4, id.length)
+            $window.open("http://cancer.sanger.ac.uk/cosmic/gene/overview?ln=" + id.toLowerCase(), "_blank");
+            $window.focus();
+        };
+
+        ctrl.openCosmicFusionId = function (id) {
+            var numericId = id.substring(id.indexOf("_") - 3, (id.length - 2));
+
+            if (numericId !== null) {
+                $window.open("http://cancer.sanger.ac.uk/cosmic/gene/overview?ln=" + numericId.toLowerCase(), "_blank");
+            }
+            $window.focus();
+        };
+        //COSMIC LINKS        
     }
 
     angular.module('matchbox').component('variantReportFiltered', {
---
+++ app/scripts/directives/cosmic_link_directive.js
@@ -0,0 +1,68 @@
+(function () {
+    "use strict";
+
+    angular
+        .module('matchbox')
+        .directive('cosmicLink', cosmicLink);
+
+    function cosmicLink($log) {
+        var controller = function () {
+            var vm = this;
+
+            vm.urls = {
+                cosmicGene: { url: 'http://cancer.sanger.ac.uk/cosmic/gene/overview?ln={id}' },
+                cosmicId: { url: 'http://cancer.sanger.ac.uk/cosmic/gene/overview?ln={id}'},
+                cosmicFusionId: { url: 'http://cancer.sanger.ac.uk/cosmic/gene/overview?ln={id}' }
+            };
+
+            vm.getLinkUrl = function() {
+                if (vm.linkType in vm.urls) {
+                    return vm.urls[vm.linkType].url.replace(/{id}/g, vm.getLinkId());
+                } else {
+                    $log.error('Invalid link type ' + vm.linkType);
+                    return '#';
+                }
+            }
+
+            vm.getLinkId = function() {
+                switch (vm.linkType) {
+                    case 'cosmicGene': 
+                        return vm.linkId;
+
+                    case 'cosmicId': 
+                        return vm.linkId.substring(4, vm.linkId.length);
+
+                    case 'cosmicFusionId': 
+                        var numericId = vm.linkId.substring(vm.linkId.indexOf("_") - 3, (vm.linkId.length - 2));
+                        if (numericId!==null) {
+                            return numericId;
+                        } else {
+                            $log.error('Invalid value for type Cusmin Fusion ID ' + vm.linkId);
+                            return '';
+                        }
+
+                    default:
+                        $log.error('Invalid link type ' + vm.linkType);
+                        return '';
+                }
+            }
+        };
+
+        /*jshint multistr: true */
+        var template = '<span><a href="{{vm.getLinkUrl()}}" target="_blank">{{vm.text | dashify}}</a></span>';
+        
+        return {
+            bindToController: true,
+            restrict: 'EA',
+            template: template,
+            controller: controller,
+            controllerAs: 'vm',
+            scope: {
+                linkId: '<',
+                linkType: '<',
+                text: '<'
+            }
+        }
+    }
+
+} ());
